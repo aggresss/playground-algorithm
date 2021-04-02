d451e156c0a9c42de094138f369bb8e4f5682101
aggresss <aggresss@163.com>
2021-04-02 21:38:00 +0800

    2021-04-02 21:38:00 CST W13D5

--- rbtree/augment/order-statistic-tree/ostree_test.c
+++ rbtree/augment/order-statistic-tree/ostree_test.c
@@ -5,13 +5,13 @@
 #include <stdio.h>
 #include <string.h>
 
-static int nnodes = 64;
+static int nnodes = 256;
 
 static bool check_argmented(struct rb_node *rb) {
-	if (!rb) {
-		return false;
-	}
-	uint32_t augmented = 1;
+    if (!rb) {
+        return false;
+    }
+    uint32_t augmented = 1;
     if (rb->rb_left) {
         augmented += rb_entry(rb->rb_left, struct ostree_node, rb)->augmented;
     }
@@ -19,7 +19,7 @@ static bool check_argmented(struct rb_node *rb) {
         augmented += rb_entry(rb->rb_right, struct ostree_node, rb)->augmented;
     }
 
-	return rb_entry(rb, struct ostree_node, rb)->augmented == augmented;
+    return rb_entry(rb, struct ostree_node, rb)->augmented == augmented;
 }
 
 void ostree_test() {
@@ -28,12 +28,16 @@ void ostree_test() {
     struct ostree_node *nodes = NULL;
     int i;
 
+    // Setup
     nodes = calloc(nnodes, sizeof(struct ostree_node));
     assert(nodes);
     memset(nodes, 0x00, nnodes * sizeof(struct ostree_node));
-
     for (i = 0; i < nnodes; i++) {
-		nodes[i].key = i;
+        nodes[i].key = i;
+    }
+
+    // Test insert
+    for (i = 0; i < nnodes; i++) {
         ostree_insert(&nodes[i], &root);
     }
     i = 0;
@@ -43,9 +47,25 @@ void ostree_test() {
     }
     for (i = 0; i < nnodes; i++) {
         assert(&nodes[i] == ostree_select(&root, i + 1));
-		assert(i + 1 == ostree_rank(&root, &nodes[i]));
+        assert(i + 1 == ostree_rank(&root, &nodes[i]));
     }
 
+    // Test remove
+    for (i = 0; i < nnodes; i += 2) {
+        ostree_remove(&nodes[i], &root);
+    }
+    i = 0;
+    for (rb = rb_first(&root.rb_root); rb; rb = rb_next(rb)) {
+        assert(rb_entry(rb, struct ostree_node, rb) == &nodes[i + 1]);
+        assert(check_argmented(rb));
+        i += 2;
+    }
+    for (i = 0; i < nnodes; i += 2) {
+        assert(&nodes[i + 1] == ostree_select(&root, (i + 2) / 2));
+        assert((i + 2) / 2 == ostree_rank(&root, &nodes[i + 1]));
+    }
+
+    // Teardown
     free(nodes);
 }
 
